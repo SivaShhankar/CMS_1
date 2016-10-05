4080097352d2eb32ac9bbd2f7f22538017c0f525
gowrisankar Gnanasekaran <gowrisankar Gnanasekaran>
2016-10-05 12:12:05 +0530

    port changed

--- Main.go
+++ Main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	config "github.com/SivaShhankar/CMS_1/Database"
 	routers "github.com/SivaShhankar/CMS_1/Routers"
@@ -28,17 +26,17 @@ func main() {
 	log.Println("Listening...")
 
 	// Listen the server.
-	http.ListenAndServe(GetPort(), mux)
+	http.ListenAndServe(":8080", mux)
 }
 
-// GetPort -- get the Port from the Dynamic environment
-func GetPort() string {
-	var port = os.Getenv("PORT")
-	if port == "" {
-		port = "4747"
-		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
-	}
+// // GetPort -- get the Port from the Dynamic environment
+// func GetPort() string {
+// 	var port = os.Getenv("PORT")
+// 	if port == "" {
+// 		port = "4747"
+// 		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+// 	}
 
-	fmt.Println("Running Port is" + port)
-	return ":" + port
-}
+// 	fmt.Println("Running Port is" + port)
+// 	return ":" + port
+// }
